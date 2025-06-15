d0571a342ddfe7baba6732c3339749e90e995b3a
DPASCII <davidcpascual@gmail.com>
2025-06-15 14:29:40 +0800

    formatted

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,18 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
-
-        "storybook-addon-theme-provider",
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-};
-export default config;
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@chromatic-com/storybook',
+        '@storybook/addon-docs',
+        '@storybook/addon-onboarding',
+        '@storybook/addon-a11y',
+        '@storybook/addon-vitest',
+        'storybook-addon-theme-provider',
+    ],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {},
+    },
+}
+export default config
