9dbe051df6665389e80d39313dfa13e8e1d89b57
DPASCII <davidcpascual@gmail.com>
2025-06-15 14:29:24 +0800

    merge main

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,7 +10,9 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
+
+        "storybook-addon-theme-provider",
   ],
   "framework": {
     "name": "@storybook/react-vite",
