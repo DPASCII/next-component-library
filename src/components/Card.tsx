b6b7a4c9a5ead55e78404c99e634c52c13c5b9a4
DPASCII <davidcpascual@gmail.com>
2025-06-17 16:59:13 +0800

    add style to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,60 @@
 'use client'
 import styled from 'styled-components'
 
-const CardWrapper = styled.div``
+const getThemeColors =
+    (prop: string) =>
+    ({ theme }: any) =>
+        theme.colors[prop]
 
-const ImageWrapper = styled.div``
+const CardWrapper = styled.div<{ $height: number; $width: number }>`
+    position: absolute;
+    display: grid;
+    grid-template-rows: 1fr 0.3fr;
+    grid-template-columns: 1fr;
+    height: ${({ $height }) => $height}px;
+    width: ${({ $width }) => $width}px;
+    border: 1px solid black;
+`
 
-const TextWrapper = styled.div``
+const StyledImage = styled.img`
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    object-position: top;
+    transition: all linear 0.3s;
+    transform: translateY(3%);
+    &:hover {
+        transform: translateY(0%);
+    }
+`
+
+const TextWrapper = styled.div`
+    z-index: 1;
+    position: relative;
+    bottom: 0;
+    padding: 16px;
+    background-color: ${getThemeColors('secondary')};
+    color: ${getThemeColors('secondaryText')};
+    overflow: hidden;
+`
 
 const Card = ({
+    height = 500,
+    width = 500,
     content,
     src,
     alt = 'card image',
 }: {
+    height: number
+    width: number
     content: string
     src: string
-    alt: string
+    alt?: string
 }) => {
     return (
-        <CardWrapper>
-            <ImageWrapper>
-                <img src={src} alt={alt} />
-            </ImageWrapper>
+        <CardWrapper $height={height} $width={width}>
+            <StyledImage src={src} alt={alt} />
+
             <TextWrapper>{content}</TextWrapper>
         </CardWrapper>
     )
