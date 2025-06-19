8581f32dbfe0cd7bed82c641dd981b6e19124c51
DPASCII <davidcpascual@gmail.com>
2025-06-19 16:54:35 +0800

    create install script for theme

---
+++ scripts/postinstall.js
@@ -0,0 +1,31 @@
+import fs from 'fs'
+import path from 'path'
+
+const libThemePath = path.resolve(__dirname, '../src/theme')
+const projectSrcPath = path.resolve(process.cwd(), 'src')
+const targetThemePath = path.resolve(projectSrcPath, 'theme')
+
+if (!fs.existsSync(projectSrcPath)) {
+    fs.mkdirSync(projectSrcPath, { recursive: true })
+}
+
+function copyDir(src, dest) {
+    if (!fs.existsSync(dest)) {
+        fs.mkdirSync(dest, { recursive: true })
+    }
+
+    const entries = fs.readdirSync(src, { withFileTypes: true })
+
+    for (const entry of entries) {
+        const srcEntry = path.join(src, entry.name)
+        const destEntry = path.join(dest, entry.name)
+
+        if (entry.isDirectory()) {
+            copyDir(srcEntry, destEntry)
+        } else {
+            fs.copyFileSync(srcEntry, destEntry)
+        }
+    }
+}
+
+copyDir(libThemePath, targetThemePath)
