5d456eb8d98145144cf673384b22dfa5877cacb7
DPASCII <davidcpascual@gmail.com>
2025-06-19 17:22:46 +0800

    change to cjs fix

--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -1,9 +1,5 @@
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const fs = require('fs')
+const path = require('path')
 
 const libThemePath = path.resolve(__dirname, '../src/theme')
 const projectSrcPath = path.resolve(process.cwd(), 'src')
@@ -31,5 +27,3 @@ function copyDir(src, dest) {
         }
     }
 }
-
-copyDir(libThemePath, targetThemePath)
