4c5f73058f521eeaae8edf8bfea3cecb2a7e26b5
DPASCII <davidcpascual@gmail.com>
2025-06-20 13:29:33 +0800

    add flexbility to call theme in themeRegistry

--- src/theme/elements/ThemeRegistry.tsx
+++ src/theme/elements/ThemeRegistry.tsx
@@ -4,9 +4,11 @@ import { ThemeProvider } from 'styled-components'
 import defaultTheme from './theme'
 
 export default function ThemeRegistry({
+    theme = defaultTheme,
     children,
 }: {
+    theme?: typeof defaultTheme
     children: React.ReactNode
 }) {
-    return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>
 }
