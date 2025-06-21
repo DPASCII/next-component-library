3c0c8d123dea5a8af5fb9fbe15d733ee64fd3b0e
DPASCII <davidcpascual@gmail.com>
2025-06-21 15:53:59 +0800

    Revert "add card container in index"
    This reverts commit 0fcebc1a6c2aae4ed2d6fed2fd3109a3afa301db.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from './components/Button'
 export { default as Header } from './components/Header'
 export { default as Card } from './components/Card'
-export { default as CardContainer } from './components/CardContainer'
 export * from './theme'
