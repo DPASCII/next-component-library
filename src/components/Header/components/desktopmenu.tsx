70d508bd8ea946036f94258b5d7d644fcde25460
DPASCII <davidcpascual@gmail.com>
2025-06-21 16:04:54 +0800

    Revert "add link href to menu item"
    This reverts commit 11d5836e008dd173dad24c8ba33d9b567a90375f.

--- src/components/Header/components/desktopmenu.tsx
+++ src/components/Header/components/desktopmenu.tsx
@@ -71,9 +71,7 @@ const DesktopMenu: React.FC<DesktopMenuProps> = (props) => {
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
-            <MenuItem $menuWidth={menuWidth} href={page.link}>
-                {page.item || 'NO ITEM'}
-            </MenuItem>
+            <MenuItem $menuWidth={menuWidth}>{page.item || 'NO ITEM'}</MenuItem>
             {isDropdownVisible && (
                 <SubMenuWrapper>
                     {page.subPages &&
