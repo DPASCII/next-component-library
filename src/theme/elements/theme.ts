aed1225d2cec4a40cc89577f87db4fe1981d04f4
David Pascual <124684893+DPASCII@users.noreply.github.com>
2025-06-21 16:08:20 +0800

    Revert "Add elements in theme"

--- src/theme/elements/theme.ts
+++ src/theme/elements/theme.ts
@@ -2,22 +2,17 @@ import { DefaultTheme } from 'styled-components'
 
 export const theme: DefaultTheme = {
     colors: {
-        primary: '#B6D0E2',
-        secondary: '#B5C7EB',
+        primary: '#0070f3',
+        secondary: '#1c1c1e',
         hoverBackground: 'white',
         button: '#007bff',
         buttonText: '#000',
         buttonHover: '#0056b3',
         buttonHoverText: '#fff',
         text: 'black',
-        secondaryText: 'black',
+        secondaryText: 'white',
     },
     windowWidth: 1320,
-    breakpoints: {
-        desktop: 1366,
-        tablet: 768,
-        mobile: 360,
-    },
 }
 
 export default theme
