8a32656729cb01c96b47bf5ee44740faf2ca683b
DPASCII <davidcpascual@gmail.com>
2025-06-27 18:32:31 +0800

    separate titleblock, added image slides

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -1,83 +1,88 @@
 'use client'
 import styled from 'styled-components'
 import Button from '../Button'
+import TitleBlock from './components/titleblock'
+import { useEffect, useState } from 'react'
 
 const getThemeColors =
     (prop: string) =>
     ({ theme }: any) =>
         theme.colors[prop]
 
-const getThemeBreakpoints =
-    (prop: string) =>
-    ({ theme }: any) =>
-        theme.breakpoints[prop]
-
 const JumbotronWrapper = styled.div`
+    position: relative;
     display: grid;
     place-items: center;
     height: 80vh;
     background-color: rgb(${getThemeColors('primary')});
+    overflow: hidden;
 `
 
-const TitleWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    place-content: center;
-    place-items: center;
+const BackgroundImage = styled.div<{ $image: string; $visible: boolean }>`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${({ $image }) => $image});
+    background-size: cover;
+    background-position: center;
+    opacity: ${({ $visible }) => ($visible ? 1 : 0)};
+    transform: scale(${({ $visible }) => ($visible ? 1.1 : 1)});
+    transition: opacity 1s ease-in-out, transform 5s ease-in-out;
+    z-index: 0;
+`
+
+const ContentWrapper = styled.div`
+    z-index: 1;
     text-align: center;
-    gap: 1rem;
-    background-color: rgba(${getThemeColors('secondary')}, 0.7);
-    color: rgb(${getThemeColors('secondaryText')});
-    border-radius: 32px;
-    height: 70%;
-    width: calc(100% - 3rem);
-    max-width: ${({ theme }) => theme.windowWidth}px;
-    margin: 3rem;
-    padding: 0 0.5rem;
-`
-
-const TextWrapper = styled.div``
-
-const Title = styled.h1`
-    font-size: 2.5rem;
-    @media (min-width: ${getThemeBreakpoints('tablet')}px) {
-        font-size: 3.5rem;
-        h2 {
-            font-size: 1.5rem;
-        }
-    }
-`
-
-const Subtitle = styled.h2``
-
-const ChildrenWrapper = styled.div`
-    display: flex;
-    gap: 1rem;
 `
 
 const Jumbotron = ({
+    images,
     title,
     subtitle,
     children,
 }: {
+    images: string[]
     title: string
     subtitle: string
     children: React.ReactNode
-}) => (
-    <JumbotronWrapper>
-        <TitleWrapper>
-            <TextWrapper>
-                <Title>{title}</Title>
-                <Subtitle>{subtitle}</Subtitle>
-            </TextWrapper>
+}) => {
+    const [index, setIndex] = useState(0)
 
-            <ChildrenWrapper>
-                <Button label="This is Button" />
-                <Button label="This is Button" />
-                {children}
-            </ChildrenWrapper>
-        </TitleWrapper>
-    </JumbotronWrapper>
-)
+    // Preload all images once
+    useEffect(() => {
+        images.forEach((src) => {
+            const img = new Image()
+            img.src = src
+        })
+    }, [images])
+
+    // Image switch timer
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prev) => (prev + 1) % images.length)
+        }, 5000)
+
+        return () => clearInterval(interval)
+    }, [images.length])
+
+    return (
+        <JumbotronWrapper>
+            {images.map((img, i) => (
+                <BackgroundImage key={i} $image={img} $visible={i === index} />
+            ))}
+
+            <ContentWrapper>
+                <TitleBlock title={title} subtitle={subtitle}>
+                    <Button label="This is Button" />
+                    <Button label="This is Button" />
+                    {children}
+                </TitleBlock>
+            </ContentWrapper>
+        </JumbotronWrapper>
+    )
+}
 
 export default Jumbotron
