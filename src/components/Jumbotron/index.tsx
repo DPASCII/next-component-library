978df9ffa48b7b845c8e9e3ad80233716d081cea
DPASCII <davidcpascual@gmail.com>
2025-06-27 12:53:16 +0800

    intialize Jumbotron

---
+++ src/components/Jumbotron/index.tsx
@@ -0,0 +1,10 @@
+'use client'
+import styled from 'styled-components'
+
+const JumbotronWrapper = styled.div``
+
+const Jumbotron = ({}: {}) => {
+    return <JumbotronWrapper></JumbotronWrapper>
+}
+
+export default Jumbotron
