ec5adc385a7f886439ec910d93fe5a6a217ac227
DPASCII <davidcpascual@gmail.com>
2025-06-27 13:35:13 +0800

    add styling (WIP)

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -1,10 +1,46 @@
 'use client'
 import styled from 'styled-components'
 
-const JumbotronWrapper = styled.div``
+const getThemeColors =
+    (props: string) =>
+    ({ theme }: any) =>
+        theme.colors[props]
 
-const Jumbotron = ({}: {}) => {
-    return <JumbotronWrapper></JumbotronWrapper>
+const JumbotronWrapper = styled.div`
+    display: grid;
+    height: 80vh;
+    background-color: black;
+`
+
+const TitleWrapper = styled.div`
+    justify-self: center;
+    align-self: center;
+    background-color: ${getThemeColors('secondary')};
+    border-radius: 32px;
+    height: 80%;
+    width: 80%;
+    max-width: ${({ theme }) => theme.windowWidth}px;
+    margin: 3rem;
+`
+const Title = styled.h1``
+
+const Subtitle = styled.h2``
+
+const Jumbotron = ({
+    title,
+    subtitle,
+}: {
+    title: string
+    subtitle: string
+}) => {
+    return (
+        <JumbotronWrapper>
+            <TitleWrapper>
+                <Title>{title}</Title>
+                <Subtitle>{subtitle}</Subtitle>
+            </TitleWrapper>
+        </JumbotronWrapper>
+    )
 }
 
 export default Jumbotron
