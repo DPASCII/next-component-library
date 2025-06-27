4096e16787efbc52f47917b1aae0cfbcaf99ad04
DPASCII <davidcpascual@gmail.com>
2025-06-27 19:52:25 +0800

    fix footer styles

--- src/theme/elements/ThemeRegistry.tsx
+++ src/theme/elements/ThemeRegistry.tsx
@@ -13,9 +13,11 @@ const GlobalCSS = createGlobalStyle`
     font-family: system-ui, sans-serif;
   }
 
-h1, h2, h3, h4, h5, h6 {
+h1, h2, h3, h4, h5, h6, p, li, ul {
+    padding: 0;
     margin: 0;
     font-size: 1rem;
+    line-height:1;
 }
   a {
     text-decoration: none;
